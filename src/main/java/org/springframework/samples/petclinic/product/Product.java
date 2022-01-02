7045109d084b51506614545dc0f5c2622402fddd
Tomas Camero <tcamerob@gmail.com>
2022-01-02 21:12:05 +0100

    Done

--- src/main/java/org/springframework/samples/petclinic/product/Product.java
+++ src/main/java/org/springframework/samples/petclinic/product/Product.java
@@ -1,13 +1,36 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
-    Integer id;
+@Entity
+@Table(name = "product")
+public class Product extends BaseEntity{
+    
+    @NotEmpty
+    @Size(min = 3, max = 50)
+    @Column(name = "name")
     String name;
+
+    @NotNull
+    @Min(0)
+    @Column(name = "price")
     double price;
+
+    @ManyToOne
     ProductType productType;
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,56 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.user.AuthoritiesService;
+import org.springframework.samples.petclinic.user.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class ProductController {
+    
+    private static final String VIEWS_OWNER_CREATE_OR_UPDATE_FORM = "products/createOrUpdateProductForm";
+
+
+    private final ProductService productService;
+
+	@Autowired
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+    
+    @GetMapping(path = "/product/create")
+    public String create(ModelMap modelMap){
+        Product product = new Product();
+        modelMap.put("product", product);
+        return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping(value = "/product/create")
+	public String processCreationForm(@Valid Product product, BindingResult result, ModelMap modelmap) {
+		if (result.hasErrors()) {
+            modelmap.put("product", product);
+            if(product.price<0){
+                return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+
+            }
+            if(product.name == ""){
+                return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+            }
+
+			return "welcome";
+		}
+		else {
+			//creating owner, user and authorities
+			this.productService.save(product);
+			
+			return VIEWS_OWNER_CREATE_OR_UPDATE_FORM;
+		}
+	}
     
 }
